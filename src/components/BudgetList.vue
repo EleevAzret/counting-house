e0e7848ab4ba8382e86be01fe29df1fabcd6fcda
Азрет Елеев <azreteleev@MacBook-Air-Azret.local>
2023-02-08 00:01:01 +0300

    added base settings for budget-list component

---
+++ src/components/BudgetList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="budget-list">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BudgetList',
+  props: {
+    list: {
+      type: Object,
+      default: () => ({}),
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
